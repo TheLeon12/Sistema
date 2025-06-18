224e4a20655250ea34cd85a2546a80e013bf58fa
Alexander <alexa@DESKTOP-ITEJDQK>
2025-06-17 23:55:51 -0400

    Inico

--- CapaEntidad/Form1.Designer.cs
+++ CapaEntidad/Form1.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace CapaPresentacion
+﻿namespace CapaEntidad
 {
     partial class Form1
     {
@@ -28,17 +28,10 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.SuspendLayout();
-            // 
-            // Form1
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.components = new System.ComponentModel.Container();
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1000, 454);
-            this.Name = "Form1";
+            this.ClientSize = new System.Drawing.Size(800, 450);
             this.Text = "Form1";
-            this.ResumeLayout(false);
-
         }
 
         #endregion
