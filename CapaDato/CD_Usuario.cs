2d18cd7da2e134aaec8e76e6d0090a16e4312500
Alexander <alexa@DESKTOP-ITEJDQK>
2025-06-18 11:55:08 -0400

    Modelo de datos

--- CapaDato/CD_Usuario.cs
+++ CapaDato/CD_Usuario.cs
@@ -4,9 +4,12 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using System.Data;
+using System.Data.SqlClient;
+
 namespace CapaDato
 {
-    public class Class1
+    public class CD_Usuario
     {
     }
 }
